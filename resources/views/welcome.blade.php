bb1d15a8ad6975885f7f5cfb4e5f7b5c0fae9f1f
ariyanbadra05-ctrl <ariyanbadra05@gmail .com>
2026-04-14 21:59:28 +0700

    tugas P4

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Praktikum 5</title>
+
+    <!-- Link CSS -->
+    <link rel="stylesheet" href="{{ asset('styles/style_arian.css') }}">
+</head>
+<body>
+
+    <h1>Ini Gambar Produk</h1>
+
+    <!-- Gambar -->
+    <img src="{{ asset('images/enak.jpg') }}" alt="produk1">
+    <img src="{{ asset('images/images.png') }}" alt="produk2">
+
+</body>
+</html>
